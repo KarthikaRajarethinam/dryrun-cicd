f5ce8a91246256b73c32957c3b063ad43af63de5
KarthikaRajarethinam <karthika.rajarethinam@igreendata.com.au>
2025-09-01 01:11:28 +0530

    Update main.ts

--- cloud-dry-run/backend/microservice-b/src/main.ts
+++ cloud-dry-run/backend/microservice-b/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-//new comment added
+//new comment 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
